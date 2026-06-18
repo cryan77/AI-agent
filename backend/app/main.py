c56ab25a8a911c5a21dba640d7c909e21823fc98
cryan77 <cryan12068@gmail.com>
2026-06-18 08:14:25 -0500

    update cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -111,7 +111,7 @@ app = FastAPI(title="AI Refund Agent", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://ai-agent-production-02bf.up.railway.app:8080"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://ai-agent-production-02bf.up.railway.app:8080", "https://ai-agent-neon-phi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
